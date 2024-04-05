d0f7c50e83ecbe416ea04c9af9964c26c6590645
Maslov-Artem <134642003+Maslov-Artem@users.noreply.github.com>
2024-04-05 20:56:30 +0300

    Add docker-compose.yml

--- Telegram/bot.py
+++ Telegram/bot.py
@@ -3,18 +3,13 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-
 from config_reader import config
-from handlers import my_conditions, cian_id_search, base, random
+from handlers import base, cian_id_search, my_conditions, random
 
-TOKEN=config.bot_token.get_secret_value()
-
-
-
+TOKEN = config.bot_token.get_secret_value()
 
 
 async def main(TOKEN=TOKEN):
-
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
@@ -22,7 +17,9 @@ async def main(TOKEN=TOKEN):
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     )
-    dp.include_routers(base.router, cian_id_search.router, my_conditions.router, random.router)
+    dp.include_routers(
+        base.router, cian_id_search.router, my_conditions.router, random.router
+    )
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
