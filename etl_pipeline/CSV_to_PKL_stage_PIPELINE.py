dc6c89c4a205c18d338fcb018d4e91edb129605e
AlexeyKamaev <jeydipak@gmail.com>
2024-04-08 15:24:51 +0300

    update

--- etl_pipeline/CSV_to_PKL_stage_PIPELINE.py
+++ etl_pipeline/CSV_to_PKL_stage_PIPELINE.py
@@ -17,6 +17,9 @@ sklearn.set_config(transform_output="pandas")
 import warnings
 warnings.filterwarnings('ignore')
 
+
+# FUNCTIONS
+
 def strr(x):
     x = x.lower()
     x = x.replace('ё','е')
@@ -45,8 +48,9 @@ def strr(x):
     x = x.replace('гольяново','щелковская')
     x = x.replace('кавказский бульвар','царицыно')
     x = x.replace('тютчевская','коньково')
-
     x = x.strip()
+
+    
     return x
 
 
@@ -60,7 +64,10 @@ except:
     eur = 100
 
 def dist(c1,c2):
-    return dis.geodesic(c1, c2).km
+    try:
+        return dis.geodesic(c1, c2).km
+    except:
+        return pd.NA
 
 def dist_maker_df(df: pd.DataFrame):
     dists = {'metro_dist': (0.0, 0.0),
@@ -77,10 +84,17 @@ def dist_maker_df(df: pd.DataFrame):
         
         if place == 'metro_dist':
             
-            df[place] = [dist(c1, c2) for c1, c2 in tqdm(df[['coord_house','coords']].itertuples(index=False))]
+            df[place] = [dist(c1, c2) for c1, c2 in df[['coord_house','coords']].itertuples(index=False)]
         else:
             
-            df[place] = [dist(c1, coord) for c1 in tqdm(df['coord_house'])]
+            df[place] = [dist(c1, coord) for c1 in df['coord_house']]
+
+
+
+###################################################################################################################################################################################
+
+
+#CUSTOM IMPUTERS
 
 
 class Coord_imp(BaseEstimator, TransformerMixin): 
@@ -108,7 +122,7 @@ class Metro_Names(BaseEstimator, TransformerMixin):
             return metro_.loc[metro_['dist']==min(metro_['dist'])].metro.to_list()[0]
         
         X_copy = X.copy()
-        X_copy['metro_station'] = [close(coords) if name == 'Нет данных' else name for coords, name in tqdm(X_copy[['coord_house','metro']].itertuples(index=False))]
+        X_copy['metro_station'] = [close(coords) if name == 'Нет данных' else name for coords, name in X_copy[['coord_house','metro']].itertuples(index=False)]
 
         return X_copy
 
@@ -139,6 +153,8 @@ class Dist_Maker(BaseEstimator, TransformerMixin):
         return self
         
     def transform(self, X: pd.DataFrame, y=None):
+
+        
         X_copy = X.copy()
 
         dist_maker_df(X_copy)
@@ -209,14 +225,149 @@ class Some_transformations(BaseEstimator, TransformerMixin):
         return X_copy
 
 
+class NA_OUT(BaseEstimator, TransformerMixin): 
+    
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X: pd.DataFrame, y=None):
+        
+        X_copy = X.copy()
+
+        X_copy = X_copy.dropna(subset=['coords'])
+
+        return X_copy
 
 
-#########################################################################################################################################################################################
 
+class LargeImputers(BaseEstimator, TransformerMixin): 
+    
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X: pd.DataFrame, y=None):
+        X_copy = X.copy()
+
+        def rooms(area):
+            if area <= 40:
+                return 1
+            elif area <= 80:
+                return 2
+            elif area <= 120:
+                return 3
+            elif area <= 160:
+                return 4
+            return 5
+        
+        X_copy['total_area'] = X_copy['total_area'].astype(float)
+        
+        X_copy['room_count'] = [rooms(area) if np.isnan(room) or room in [0,'0','False',False,'Нет данных'] else room for area, room in X_copy[['total_area','room_count']].itertuples(index=False)]
+        ############################################################################################################################
+        # ПОТОЛОК
+        ############################################################################################################################
+        height = X_copy.loc[X_copy.ceiling_height != 'Нет данных'].ceiling_height.astype(float).median()
+
+        def ceil(x):
+            if x == 'Нет данных':
+                return height
+            x = float(x)
+
+            if x <= 1.5:
+                return height
+            return x
+
+        X_copy['ceiling_height'] = X_copy['ceiling_height'].map(ceil)
+
+        ############################################################################################################################
+        # ПОТОЛОК
+        ############################################################################################################################
+
+        ############################################################################################################################
+        # ГОД
+        ############################################################################################################################
+
+        def year(year):
+            if pd.isna(year) or year == 'Нет данных' or year == 0:
+                return pd.NA
+            else:
+                return int(year)
+                
+        X_copy['build_year'] = X_copy['build_year'].map(year)
+
+        def year_inp(x):
+            try:
+                return metro_year.loc[metro_year.index == x].iloc[0].item()
+            except:
+                return 0
+        
+        
+        X_copy['build_year'] = [year_inp(station) if pd.isna(year) or year <= 1500 or year in ['Нет данных ',0,False] else year for year, station in X_copy[['build_year','metro_m']].itertuples(index=False)]
+
+
+        ############################################################################################################################
+        # ГОД
+        ############################################################################################################################
+
+        def repair_imp(x):
+            if pd.isna(x) or x in ['Нет данных',0,'0',False]:
+                return 'no'
+            return x
+        
+        X_copy['repair_type'] = X_copy['repair_type'].map(repair_imp)
+        return X_copy
+
+
+
+class Material_IMP(BaseEstimator, TransformerMixin): 
+    
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X: pd.DataFrame, y=None):
+        X_copy = X.copy()
+
+        def typer(year):
+            if year <= 1963:
+                return 'brick'
+            elif year <= 1995:
+                return 'panel'
+            return 'monolith'
+        
+        X_copy['material_type'] = [typer(year) if mtype in [0,'0','False',False,'Нет данных'] or pd.isna(mtype) else mtype for year, mtype in X_copy[['build_year','material_type']].itertuples(index=False)]
+
+        return X_copy
+
+
+class Okrug_Normalizer(BaseEstimator, TransformerMixin): 
+    
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X: pd.DataFrame, y=None):
+        X_copy = X.copy()
+
+        def func(station):
+            okr = metro_okrug.loc[metro_okrug.metro_m == station].new_okrug.to_list()[0]
+            if type(okr)==str:
+                return okr
+            return okr[0]
+
+        X_copy.okrug = [func(station) if "АО" not in distr else distr for station, distr in tqdm(X_copy[['metro_m','okrug']].itertuples(index=False))]
+        return X_copy
+
+
+
+
+
+###################################################################################################################################################################
+#PIPELINES
+
+#1
+
+#########################################################################
 coord_list = ['latitude','longitude']
 metro_list = ['coord_house','metro']
 final_stage = ['house_heating_supply','is_apartment','currency','price']
-to_drop = ['currency','coords']
 
 first_step = ColumnTransformer(
     transformers=[
@@ -262,12 +413,6 @@ sixth_step = ColumnTransformer(
      verbose_feature_names_out = False,
      remainder = 'passthrough')
 
-seventh_step = ColumnTransformer(
-    transformers=[
-        ('final_dropper', 'drop', to_drop),
-    ],
-     verbose_feature_names_out = False,
-     remainder = 'passthrough')
 
 preprocessor = Pipeline(
     [
@@ -277,124 +422,11 @@ preprocessor = Pipeline(
         ('distance_maker', fourth_step),
         ('smart_dropper', fifth_step),
         ('little_transforms', sixth_step),
-        ('dropper_2', seventh_step)
     ]
 )
+#########################################################################
 
-###########################################################################################################################################################################################
-#
-#
-#
-#
-#
-#stage 2 stage 2 stagw 2
-#
-#
-#
-#
-#
-#
-########################################################################################################################################################################################
-class LargeImputers(BaseEstimator, TransformerMixin): 
-    
-    def fit(self, X, y=None):
-        return self
-        
-    def transform(self, X: pd.DataFrame, y=None):
-        X_copy = X.copy()
-
-        def rooms(area):
-            if area <= 40:
-                return 1
-            elif area <= 80:
-                return 2
-            elif area <= 120:
-                return 3
-            elif area <= 160:
-                return 4
-            return 5
-        
-        X_copy['total_area'] = X_copy['total_area'].astype(float)
-        
-        X_copy['room_count'] = [rooms(area) if np.isnan(room) or room in [0,'0','False',False,'Нет данных'] else room for area, room in X_copy[['total_area','room_count']].itertuples(index=False)]
-
-        height = X_copy.loc[X_copy.ceiling_height != 'Нет данных'].ceiling_height.astype(float).median()
-
-        def ceil(x):
-            if x == 'Нет данных':
-                return height
-            x = float(x)
-
-            if x <= 1.5:
-                return height
-            return x
-
-        X_copy['ceiling_height'] = X_copy['ceiling_height'].map(ceil)
-
-        def f(year):
-            if year == 'Нет данных':
-                return np.NaN
-            else:
-                return int(year)
-                
-        X_copy['build_year'] = X_copy['build_year'].map(f)
-
-        def ff(x):
-            if x == 'боровицкая':
-                x = 'арбатская'
-            return metro_year.loc[metro_year.index == x].iloc[0].item()
-        
-        
-        X_copy['build_year'] = [ff(station) if year <= 1500 or np.isnan(year) or year in ['Нет данных ',0,'0',False] else year for year, station in X_copy[['build_year','metro_m']].itertuples(index=False)]
-
-        def repair_imp(x):
-            if x in ['Нет данных',0,'0',False] or pd.isna(x):
-                return 'no'
-            return x
-        
-        X_copy['repair_type'] = X_copy['repair_type'].map(repair_imp)
-        return X_copy
-
-
-
-class Material_IMP(BaseEstimator, TransformerMixin): 
-    
-    def fit(self, X, y=None):
-        return self
-        
-    def transform(self, X: pd.DataFrame, y=None):
-        X_copy = X.copy()
-
-        def typer(year):
-            if year <= 1963:
-                return 'brick'
-            elif year <= 1995:
-                return 'panel'
-            return 'monolith'
-        
-        X_copy['material_type'] = [typer(year) if mtype in [0,'0','False',False,'Нет данных'] or pd.isna(mtype) else mtype for year, mtype in X_copy[['build_year','material_type']].itertuples(index=False)]
-
-        return X_copy
-
-
-class Okrug_Normalizer(BaseEstimator, TransformerMixin): 
-    
-    def fit(self, X, y=None):
-        return self
-        
-    def transform(self, X: pd.DataFrame, y=None):
-        X_copy = X.copy()
-
-        def func(station):
-            okr = metro_okrug.loc[metro_okrug.metro_m == station].new_okrug.to_list()[0]
-            if type(okr)==str:
-                return okr
-            return okr[0]
-
-        X_copy.okrug = [func(station) if "АО" not in distr else distr for station, distr in X_copy[['metro_m','okrug']].itertuples(index=False)]
-        return X_copy
-        
-
+#2
 
 last_stage = ['total_area','room_count','ceiling_height','build_year','metro_m','repair_type']
 
@@ -422,40 +454,76 @@ preprocessor_stage_2 = Pipeline(
     ]
 )
 
+###################################################################################################################################################################
 
-metro = pd.read_pickle('metro.pkl')
-metro_year = pd.read_pickle('metro_year.pkl')
-metro_okrug = pd.read_pickle('metro_okrug.pkl')
-#
-#
-#
-#
-#
-########################################################################################################################################################################################
-#1
-
-data = pd.read_csv('output.csv').drop_duplicates(subset=['id']).set_index('id')
-
-#2
-
-data_pre = preprocessor.fit_transform(data)
-data_pre.index = data.index.to_list()
-
-#3 making 28 columns
-
-if 'room_count' not in data_pre.columns:
-    data_pre['room_count'] = np.NaN
-
-#4 stage 2
+#PREPROCESSING
 
 
-data_pre_2 = preprocessor_stage_2.fit_transform(data_pre)
+from functools import wraps
+import time
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
 
 
 
-#5 saving
+class Preprocessor():
+    
+    def __init__(self, path):
 
-ct = datetime.datetime.now()
-title = f'data_{str(ct)[:str(ct).find(".")].replace(" ","_")}.pkl'
+        global metro, metro_year, metro_okrug
 
-data_pre_2.to_pickle(title)
+        metro = pd.read_pickle('data/metro.pkl')
+        metro_year = pd.read_pickle('data/metro_year.pkl')
+        metro_okrug = pd.read_pickle('data/metro_okrug.pkl')
+
+        self.path = path
+        self.data = pd.read_csv(path).drop_duplicates(subset=['id']).set_index('id')
+
+        
+    @timeit
+    def transform(self):
+
+
+
+        self.data_pre = preprocessor.fit_transform(self.data)
+        self.data_pre.index = self.data.index.to_list()
+        self.data_pre = self.data_pre.dropna(subset=['coords']).drop(['currency','coords'],axis=1)
+
+        if 'room_count' not in tqdm(self.data_pre.columns):
+            self.data_pre['room_count'] = np.NaN
+
+        self.data_pre = preprocessor_stage_2.fit_transform(self.data_pre)
+        self.data_pre = self.data_pre[self.data_pre.build_year != 0]
+        self.data_pre.metro_dist = self.data_pre.metro_dist.astype(float)
+
+    def to_pandas(self):
+
+        return self.data_pre
+
+    def save(self, title = None):
+
+        if title is None:
+            ct = datetime.datetime.now()
+            title = f'data_{str(ct)[:str(ct).find(".")].replace(" ","_")}.pkl'
+
+        try:
+
+            try: 
+                self.data_pre.to_pickle(title)
+                print(f'Записи обработаны. Файл сохраненен как {title}')
+            except:
+                self.data_pre.to_pickle(title+'.pkl')
+                print(f'Записи обработаны. Файл сохраненен как {title}')
+        except:
+            
+            print('You should call meth. transform first')
